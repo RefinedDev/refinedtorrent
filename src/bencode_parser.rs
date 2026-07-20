194219cf4dc7a16d583514a32498d77869b1ca1f
RefinedDev <aryamkkumar2005@gmail.com>
2026-07-20 11:40:48 +0530

    &str -> &[u8] because .torrent files can give invalid utf8 bytes

--- src/bencode_parser.rs
+++ src/bencode_parser.rs
@@ -1,49 +1,71 @@
-use serde_json::{Map, Value};
 use anyhow::{Result, anyhow};
+use serde_json::{Map, Value};
 
 // Strings are of the form "<len>:<string>"
-// This returns the colon index and length respectively
-fn decode_str(encoded: &str) -> Result<(usize, usize)> {
-    let colon_index = encoded.find(':').ok_or_else(|| anyhow!("No colon found in beginning of {}", encoded))?;
-    return Ok((colon_index, encoded[..colon_index].parse::<usize>()?))
+// This returns the colon index and length of string respectively
+fn decode_str(encoded: &[u8]) -> Result<(usize, usize)> {
+    let colon_index = encoded.iter().position(|b| b == &b':').ok_or_else(|| {
+        anyhow!(
+            "No colon found in beginning of {}",
+            String::from_utf8_lossy(encoded)
+        )
+    })?;
+    return Ok((
+        colon_index,
+        str::from_utf8(&encoded[..colon_index])?.parse::<usize>()?,
+    ));
 }
 
 // Numbers are of the form "i<number>e"
-// This returns the number and length of the number in string form 
+// This returns the number and length of the number in string form
 // For example "120" has a length of 3
-fn decode_number(encoded: &str) -> Result<(i64, usize)> {
-    let number_string = &encoded[1..encoded.find("e").ok_or_else(|| anyhow!("No number found in beginning of {}", encoded))?];
+fn decode_number(encoded: &[u8]) -> Result<(i64, usize)> {
+    let number_string = str::from_utf8(
+        &encoded[1..encoded.iter().position(|b| b == &b'e').ok_or_else(|| {
+            anyhow!(
+                "No number found in beginning of {}",
+                String::from_utf8_lossy(encoded)
+            )
+        })?],
+    )?;
     return Ok((number_string.parse::<i64>()?, number_string.len()));
 }
 
 // Lists are of the form "l<item1><item2>...<itemN>e"
 // This returns the decoded list and an optional string which is only Some() during recursion (it is the rest string)
-fn decode_list(mut rest: &str) -> Result<(Vec<Value>, Option<&str>)> {
+fn decode_list(mut rest: &[u8]) -> Result<(Vec<Value>, Option<&[u8]>)> {
     let mut list: Vec<Value> = Vec::new();
     while !rest.is_empty() {
-        let first_char = rest.chars().next().ok_or_else(|| anyhow!("bencode string is empty!"))?;
-        if first_char.is_ascii_digit() { // string
+        let first_char = rest
+            .get(0)
+            .ok_or_else(|| anyhow!("bencode string is empty!"))?;
+        if first_char.is_ascii_digit() {
+            // string
             let (colon_idx, strlen) = decode_str(rest)?;
             let string = &rest[colon_idx + 1..colon_idx + 1 + strlen];
-            rest = &rest[colon_idx+strlen+1..];
+            rest = &rest[colon_idx + strlen + 1..];
             list.push(string.into());
-        } else if first_char == 'i' { // number
+        } else if first_char == &b'i' {
+            // number
             let (number, len) = decode_number(rest)?;
-            rest = &rest[len+2..];
+            rest = &rest[len + 2..];
             list.push(number.into());
-        } else if first_char == 'l' { // list
-            rest = rest.strip_prefix("l").unwrap();
+        } else if first_char == &b'l' {
+            // list
+            rest = rest.strip_prefix(b"l").unwrap();
             let recursed = decode_list(rest)?;
             list.push(recursed.0.into());
             rest = recursed.1.unwrap()
-        } else if first_char == 'd' { // dict
-            rest = rest.strip_prefix("d").unwrap();
+        } else if first_char == &b'd' {
+            // dict
+            rest = rest.strip_prefix(b"d").unwrap();
             let recursed = decode_dict(rest)?;
             list.push(recursed.0.into());
             rest = recursed.1.unwrap()
-        } else if first_char == 'e' { // only gonna happen during recursion or if the bencode is faulty
-            rest = rest.strip_prefix("e").unwrap();
-            return Ok((list, Some(rest)))
+        } else if first_char == &b'e' {
+            // only gonna happen during recursion or if the bencode is faulty
+            rest = rest.strip_prefix(b"e").unwrap();
+            return Ok((list, Some(rest)));
         }
     }
     Ok((list, None))
@@ -51,62 +73,94 @@ fn decode_list(mut rest: &str) -> Result<(Vec<Value>, Option<&str>)> {
 
 // Dictionaries are of the form d<item1><key1><item2><key2>....<itemN><keyN>e
 // This returns the decoded dictionary and an optional string which is only Some during recursion (it is the rest string)
-fn decode_dict(mut rest: &str) -> Result<(Map<String,Value>, Option<&str>)> {
+// The official BitTorrent specification says that Dict keys are ALWAYS valid utf-8 byte strings
+// Therefore I am taking the liberty of checking and converting byte strings to dict keys
+fn decode_dict(mut rest: &[u8]) -> Result<(Map<String, Value>, Option<&[u8]>)> {
     let mut dict: Map<String, Value> = Map::new();
     let mut key: Option<String> = None;
-    let mut insert_if_can = |item: Value| {
-        if key.is_none() {
-            key = Some(item.as_str().unwrap().to_owned())
-        } else {
-            dict.insert(key.take().unwrap(), item);
-        }
-    };
-
+   
     while !rest.is_empty() {
-        let first_char = rest.chars().next().ok_or_else(|| anyhow!("bencode string is empty!"))?;
-        if first_char.is_ascii_digit() { // string
+        let first_char = rest
+            .get(0)
+            .ok_or_else(|| anyhow!("bencode string is empty!"))?;
+        if first_char.is_ascii_digit() {
+            // string
             let (colon_idx, strlen) = decode_str(rest)?;
             let string = &rest[colon_idx + 1..colon_idx + 1 + strlen];
-            rest = &rest[colon_idx+strlen+1..];
-            insert_if_can(string.into());
-        } else if first_char == 'i' { // number
+            rest = &rest[colon_idx + strlen + 1..];
+            // Check if there exists a dictionary key; if yes then this string is a Value
+            if key.is_none() {
+                key = Some(String::from_utf8(string.to_vec()).unwrap())
+            } else {
+                dict.insert(key.take().unwrap(), string.into());
+            }
+        } else if first_char == &b'i' {
+            // number
             let (number, len) = decode_number(rest)?;
-            rest = &rest[len+2..];
-            insert_if_can(number.into());
-        } else if first_char == 'l' { // list
-            rest = rest.strip_prefix("l").unwrap();
+            rest = &rest[len + 2..];
+            dict.insert(key.take().unwrap(), number.into());
+        } else if first_char == &b'l' {
+            // list
+            rest = rest.strip_prefix(b"l").unwrap();
             let recursed = decode_list(rest)?;
-            insert_if_can(recursed.0.into());
+            dict.insert(key.take().unwrap(), recursed.0.into());
             rest = recursed.1.unwrap()
-        } else if first_char == 'd' { // dict
-            rest = rest.strip_prefix("d").unwrap();
+        } else if first_char == &b'd' {
+            // dict
+            rest = rest.strip_prefix(b"d").unwrap();
             let recursed = decode_dict(rest)?;
-            insert_if_can(recursed.0.into());
+            dict.insert(key.take().unwrap(), recursed.0.into());
             rest = recursed.1.unwrap()
-        } else if first_char == 'e' { // only gonna during recursion or if the bencode is faulty
-            rest = rest.strip_prefix("e").unwrap();
-            return Ok((dict, Some(rest)))
+        } else if first_char == &b'e' {
+            // only gonna during recursion or if the bencode is faulty
+            rest = rest.strip_prefix(b"e").unwrap();
+            return Ok((dict, Some(rest)));
         }
     }
 
     Ok((dict, None))
 }
 
-pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
-    let first_char = encoded_value.chars().next().ok_or_else(|| anyhow!("bencode string is empty!"))?;
-    if first_char.is_ascii_digit() { // string
-        let (colon_idx, strlen) = decode_str(encoded_value)?;
+pub fn decode_bencoded_value(encoded_value: &[u8]) -> Result<serde_json::Value> {
+    let first_char = encoded_value
+        .get(0)
+        .ok_or_else(|| anyhow!("bencode string is empty!"))?;
+    if first_char.is_ascii_digit() {
+        // string
+        let (colon_idx, strlen) = decode_str(&encoded_value)?;
         let string = &encoded_value[colon_idx + 1..colon_idx + 1 + strlen];
-        return Ok(string.into())
-    } else if first_char == 'i' { // number
-        return Ok(decode_number(encoded_value)?.0.into())
-    } else if first_char == 'l' { // list
-        let rest = encoded_value.strip_prefix("l").and_then(|r| r.strip_suffix("e")).ok_or_else(|| anyhow!("Invalid bencode {}", encoded_value))?;
-        return Ok(decode_list(rest)?.0.into())
-    } else if first_char == 'd' { // dictionary
-        let rest = encoded_value.strip_prefix("d").and_then(|r| r.strip_suffix("e")).ok_or_else(|| anyhow!("Invalid bencode {}", encoded_value))?;
-        return Ok(decode_dict(rest)?.0.into())
+        return Ok(string.into());
+    } else if first_char == &b'i' {
+        // number
+        return Ok(decode_number(&encoded_value)?.0.into());
+    } else if first_char == &b'l' {
+        // list
+        let rest = encoded_value
+            .strip_prefix(b"l")
+            .and_then(|r| r.strip_suffix(b"e"))
+            .ok_or_else(|| {
+                anyhow!(
+                    "Invalid bencode {}",
+                    String::from_utf8_lossy(&encoded_value)
+                )
+            })?;
+        return Ok(decode_list(rest)?.0.into());
+    } else if first_char == &b'd' {
+        // dictionary
+        let rest = encoded_value
+            .strip_prefix(b"d")
+            .and_then(|r| r.strip_suffix(b"e"))
+            .ok_or_else(|| {
+                anyhow!(
+                    "Invalid bencode {}",
+                    String::from_utf8_lossy(&encoded_value)
+                )
+            })?;
+        return Ok(decode_dict(rest)?.0.into());
     } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+        panic!(
+            "Unhandled encoded value: {}",
+            String::from_utf8_lossy(&encoded_value)
+        )
     }
 }
--- src/bencode_tests.rs
+++ src/bencode_tests.rs
@@ -2,42 +2,42 @@ use crate::bencode_parser;
 
 #[test]
 fn string_test() {
-    let decoded_value = bencode_parser::decode_bencoded_value("10:helloworld").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("10:helloworld".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn number_test() {
-    let decoded_value = bencode_parser::decode_bencoded_value("i69420e").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("i69420e".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn list_test() {
-    let decoded_value = bencode_parser::decode_bencoded_value("l5:gamer4:girl3:xxxi12ee").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("l5:gamer4:girl3:xxxi12ee".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn nested_lists() {
-    let decoded_value = bencode_parser::decode_bencoded_value("lli1ei2eli3ei4eeel5:alphal4:betai5ei6el7:charliei8eeee3:ende").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("lli1ei2eli3ei4eeel5:alphal4:betai5ei6el7:charliei8eeee3:ende".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn dict_test() {
-    let decoded_value = bencode_parser::decode_bencoded_value("d5:gamer4:girl3:xxxi12ee").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("d5:gamer4:girl3:xxxi12ee".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn nested_dicts() {
-    let decoded_value = bencode_parser::decode_bencoded_value("d4:rootd6:configd5:debugi1e7:version3:1.0e4:infod3:agei20e4:name3:Bobee5:valuei42eee").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("d4:rootd6:configd5:debugi1e7:version3:1.0e4:infod3:agei20e4:name3:Bobee5:valuei42eee".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
 
 #[test]
 fn bigbencode() {
-    let decoded_value = bencode_parser::decode_bencoded_value("d4:infod4:name8:test.txt6:lengthi12345e6:piecesl6:piece16:piece26:piece3ee5:ownerd3:agei30e7:contactd5:email14:alice@test.com5:phonel8:123456788:98765432ee4:name5:Alicee4:tagsl4:rust7:bencode4:jsonl5:inneri42ed1:xi1e1:yl5:hello5:worldeeeee").unwrap();
-    println!("{}", decoded_value.to_string());
+    let decoded_value = bencode_parser::decode_bencoded_value("d4:infod4:name8:test.txt6:lengthi12345e6:piecesl6:piece16:piece26:piece3ee5:ownerd3:agei30e7:contactd5:email14:alice@test.com5:phonel8:123456788:98765432ee4:name5:Alicee4:tagsl4:rust7:bencode4:jsonl5:inneri42ed1:xi1e1:yl5:hello5:worldeeeee".as_bytes()).unwrap();
+    println!("{}", decoded_value);
 }
