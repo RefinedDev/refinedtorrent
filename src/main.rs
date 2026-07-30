1cda20ee1aa8900dfbea5c7c6b4f9f9b6f2ee879
RefinedDev <aryamkkumar2005@gmail.com>
2026-07-30 12:33:06 +0530

    Multiple Peers Single File done but some torrents like Ubuntu are not working I gotta figure out why

--- src/main.rs
+++ src/main.rs
@@ -5,150 +5,62 @@ mod peer;
 mod torrent;
 
 use anyhow::Result;
-use sha1::{Digest, Sha1};
-use std::sync::Arc;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
-use tokio::sync::Mutex;
+use std::sync::{Arc, atomic::AtomicUsize};
+use tokio::sync::{Mutex, Notify};
 
+use peer::Peer;
 use torrent::Torrent;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let bytes = std::fs::read("sample.torrent")?;
-    let torrent = Torrent::new(bencode_parser::decode(&bytes)?, "hellomynameisgamer12", "6881");
-    
+    let torrent = Torrent::new(
+        bencode_parser::decode(&bytes)?,
+        "hellomynameisgamer12",
+        "6881",
+    );
+
     let info_bencode = Arc::new(torrent.get_info_bencode());
     let ips = torrent.get_peer_ips(&info_bencode).await?;
 
-    let hashed_torrent_pieces = Arc::new(torrent.get_piece_hashes()?);
-    let hashed_obtained_pieces = Arc::new(Mutex::new(Vec::with_capacity(hashed_torrent_pieces.len())));
-    let obtained_pieces_bytes = Arc::new(Mutex::new(Vec::with_capacity(hashed_torrent_pieces.len())));
+    let hashed_torrent_pieces: Arc<Vec<String>> = Arc::new(torrent.get_piece_hashes()?);
+    let bytes_obtained_pieces =
+        Arc::new(Mutex::new(vec![vec![]; hashed_torrent_pieces.len()]));
 
-    let mut connections = Vec::with_capacity(ips.len());
-    {
-        // TODO: IMPLEMENT MULTI-PEER
-        let info_hash = Arc::clone(&info_bencode);
-        let hashed_torrent_pieces = Arc::clone(&hashed_torrent_pieces);
-        let hashed_obtained_pieces = Arc::clone(&hashed_obtained_pieces);
-        let obtained_pieces_bytes = Arc::clone(&obtained_pieces_bytes);
+    let pieces_left = Arc::new(Mutex::new((0..hashed_torrent_pieces.len()).collect()));
+    let pieces_done = Arc::new(AtomicUsize::new(0));
+    let all_done_event = Arc::new(Notify::new());
 
-        let ip = ips[0].clone();
-        let total_length = torrent.total_length.clone() as u32;
-        let piece_length = torrent.piece_length.clone() as u32;
-
-        connections.push(tokio::spawn(async move {
-            let mut stream = TcpStream::connect(ip).await.unwrap();
-
-            let mut h1 = [0u8; 68]; // To establish a connection we need to do a handshake
-            h1[0] = 19;
-            h1[1..20].copy_from_slice(b"BitTorrent protocol");
-            h1[20..28].fill(0);
-            h1[28..48].copy_from_slice(&Sha1::digest(&*info_hash));
-            h1[48..68].copy_from_slice(b"hellomynameisgamer12");
-            stream.write_all(&h1).await.unwrap();
-            let mut h2 = [0u8; 68]; // The peer returns something similar in return
-            stream.read_exact(&mut h2).await.unwrap();
-
-            // Get 'bitfield' payload
-            let mut length_bytes = [0u8; 4];
-            let mut message_id = [0u8; 1];
-            stream.read_exact(&mut length_bytes).await.unwrap();
-            stream.read_exact(&mut message_id).await.unwrap(); // message_id for bitfield is 5
-            let mut bitfield_payload_bytes =
-                vec![0u8; (u32::from_be_bytes(length_bytes) - 1) as usize]; // Message takes 1 length
-            stream
-                .read_exact(&mut bitfield_payload_bytes)
-                .await
-                .unwrap();
-
-            // Send 'interested' Message
-            let length_bytes = (1 as u32).to_be_bytes();
-            let message_id: [u8; 1] = [2];
-            stream.write_all(&length_bytes).await.unwrap();
-            stream.write_all(&message_id).await.unwrap();
-
-            // Get 'unchoke' message
-            let mut length_bytes = [0u8; 4];
-            let mut message_id = [0u8; 1];
-            stream.read_exact(&mut length_bytes).await.unwrap();
-            stream.read_exact(&mut message_id).await.unwrap(); // message id for unchoke is 1
-
-            // Send 'request' message
-            for i in 0..hashed_torrent_pieces.len() {
-                let this_piece_length = if i == hashed_torrent_pieces.len() - 1 {
-                    total_length - piece_length * (hashed_torrent_pieces.len() as u32 - 1)
-                } else {
-                    piece_length
-                };
-
-                let mut begin_offset: u32 = 0;
-
-                while begin_offset < this_piece_length {
-                    let length = std::cmp::min(16 * 1024, this_piece_length - begin_offset);
-
-                    let length_bytes = (13u32).to_be_bytes();
-                    let message_id: [u8; 1] = [6];
-                    let mut payload = [0u8; 12];
-
-                    payload[0..4].copy_from_slice(&(i as u32).to_be_bytes());
-                    payload[4..8].copy_from_slice(&begin_offset.to_be_bytes());
-                    payload[8..12].copy_from_slice(&length.to_be_bytes());
-
-                    stream.write_all(&length_bytes).await.unwrap();
-                    stream.write_all(&message_id).await.unwrap();
-                    stream.write_all(&payload).await.unwrap();
-
-                    begin_offset += length;
-                }
-            }
-
-            // Get 'piece' message and stich all pieces together
-            for i in 0..hashed_torrent_pieces.len() {
-                let this_piece_length = if i == hashed_torrent_pieces.len() - 1 {
-                    total_length - piece_length * (hashed_torrent_pieces.len() as u32 - 1)
-                } else {
-                    piece_length
-                };
-                let mut block_length = 0;
-                let mut piece_buffer = vec![0u8; this_piece_length as usize];
-                while block_length < this_piece_length {
-                    let mut length_bytes = [0u8; 4];
-                    let mut message_id = [0u8; 1];
-                    stream.read_exact(&mut length_bytes).await.unwrap();
-                    if u32::from_be_bytes(length_bytes) == 0 {
-                        continue;
-                    }
-                    stream.read_exact(&mut message_id).await.unwrap(); // message_id for piece is 7
-                    let mut payload = vec![0u8; (u32::from_be_bytes(length_bytes) - 1) as usize]; // Message takes 1 length
-                    stream.read_exact(&mut payload).await.unwrap();
-                    match message_id {
-                        [7] => {
-                            let _index = u32::from_be_bytes(payload[0..4].try_into().unwrap());
-                            let begin = u32::from_be_bytes(payload[4..8].try_into().unwrap());
-                            let block = &payload[8..];
-                            piece_buffer[begin as usize..begin as usize + block.len()]
-                                .copy_from_slice(block);
-                            block_length += block.len() as u32;
-                        }
-                        _ => (),
-                    }
-                }
-                hashed_obtained_pieces
-                    .lock()
-                    .await
-                    .push(torrent::sha1_bytes_to_hex(Sha1::digest(&piece_buffer), false).unwrap());
-                obtained_pieces_bytes.lock().await.extend(piece_buffer);
-            }
-        }));
+    let mut tasks = Vec::with_capacity(ips.len());
+    println!("Peers: {:?}", ips);
+    for ip in ips.iter() {
+        let peer = Peer::new(ip);
+        peer.connect(
+            &mut tasks,
+            Arc::clone(&info_bencode),
+            Arc::clone(&hashed_torrent_pieces),
+            Arc::clone(&bytes_obtained_pieces),
+            Arc::clone(&pieces_left),
+            Arc::clone(&pieces_done),
+            torrent.total_length.clone() as u32,
+            torrent.piece_length.clone() as u32,
+            Arc::clone(&all_done_event),
+        );
     }
 
-    for handle in connections {
-        handle.await?;
+    for task in tasks {
+        task.await?;
     }
 
-    assert_eq!(&*hashed_torrent_pieces, &*hashed_obtained_pieces.lock().await); // TODO: IF NOT MATCH THEN RETRY WITH ANOTHER PEER?
-    std::fs::write(torrent.save_file_name, &*obtained_pieces_bytes.lock().await).unwrap();
+    // We have disconnected all connections so I can safely get rid of the Mutex
+    let mut joined_bytes = Vec::new();
+    let mutex = Arc::try_unwrap(bytes_obtained_pieces).unwrap();
+    let unlocked = mutex.into_inner();
+
+    for arr in unlocked.into_iter() {
+        joined_bytes.extend(arr);
+    }
+    std::fs::write(torrent.save_file_name, joined_bytes).unwrap();
 
     Ok(())
 }
---
+++ src/peer.rs
@@ -0,0 +1,212 @@
+use std::sync::{
+    Arc,
+    atomic::{AtomicUsize, Ordering},
+};
+
+use sha1::{Digest, Sha1};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::{Mutex, Notify},
+    task::JoinHandle,
+};
+
+fn peer_has_piece(bytes: &[u8], bit: usize) -> bool {
+    let bit_index = bit / 8;
+    let bit_offset = bit % 8;
+    (bytes[bit_index] >> (7 - bit_offset)) & 1 == 1
+}
+pub struct Peer<'a> {
+    ip: &'a str,
+}
+
+impl<'a> Peer<'a> {
+    pub fn new(ip: &'a str) -> Self {
+        Peer { ip }
+    }
+
+    pub fn connect(
+        &self,
+        tasks: &mut Vec<JoinHandle<()>>,
+        info_hash: Arc<Vec<u8>>,
+        hashed_torrent_pieces: Arc<Vec<String>>,
+        bytes_obtained_pieces: Arc<Mutex<Vec<Vec<u8>>>>,
+        pieces_left: Arc<Mutex<Vec<usize>>>,
+        pieces_done: Arc<AtomicUsize>,
+        total_length: u32,
+        piece_length: u32,
+        all_done_event: Arc<Notify>,
+    ) {
+        let ip = self.ip.to_owned();
+        tasks.push(tokio::spawn(async move {
+            let mut stream = TcpStream::connect(&ip).await.unwrap();
+            let mut choked = true;
+
+            // PIECE RELATED
+            let mut current_piece_index: usize = usize::MAX;
+            let mut piece_buffer: Vec<u8> = Vec::new();
+            let mut block_length: u32 = 0;
+
+            let mut h1 = [0u8; 68]; // To establish a connection we need to do a handshake
+            h1[0] = 19;
+            h1[1..20].copy_from_slice(b"BitTorrent protocol");
+            h1[20..28].fill(0);
+            h1[28..48].copy_from_slice(&Sha1::digest(&*info_hash));
+            h1[48..68].copy_from_slice(b"hellomynameisgamer12"); // FIX THIS LATER
+            stream.write_all(&h1).await.unwrap();
+            let mut h2 = [0u8; 68]; // The peer returns something similar in return
+            stream.read_exact(&mut h2).await.unwrap();
+
+            // Get 'bitfield' payload
+            let mut length_bytes = [0u8; 4];
+            let mut message_id = [0u8; 1];
+            stream.read_exact(&mut length_bytes).await.unwrap();
+            stream.read_exact(&mut message_id).await.unwrap(); // message_id for bitfield is 5
+            let mut bitfield = vec![0u8; (u32::from_be_bytes(length_bytes) - 1) as usize]; // Message takes 1 length
+            stream.read_exact(&mut bitfield).await.unwrap();
+
+            // Send 'interested' Message
+            let length_bytes = (1 as u32).to_be_bytes();
+            let message_id: [u8; 1] = [2];
+            stream.write_all(&length_bytes).await.unwrap();
+            stream.write_all(&message_id).await.unwrap();
+
+            loop {
+                if pieces_done.load(Ordering::Relaxed) == hashed_torrent_pieces.len() {
+                    drop(stream);
+                    break;
+                }
+                
+                let mut length_bytes = [0u8; 4];
+                let mut message_id = [0u8; 1];
+
+                tokio::select! {
+                    result = stream.read_exact(&mut length_bytes) => {
+                        result.unwrap();
+                        stream.read_exact(&mut message_id).await.unwrap();
+                    }
+                    _ = all_done_event.notified() => {
+                        drop(stream);
+                        break;
+                    }
+                }
+                
+                match message_id {
+                    [0] => choked = true,
+                    [1] => choked = false,
+                    [2] => (), // peer interested but i aint seeding 👻
+                    [3] => (), // peer not interested 👻
+                    [4] => {}  // TODO: the peer has a piece we want now but didnt have earlier
+                    [5] => (), // bitfield (we already have it)
+                    [6] => (), // peer request but i aint seeding 👻
+                    [7] => {
+                        // All pieces except MAYBE the last one have the same length, the piece can be truncated and then the last won't be the same as rest
+                        let this_piece_length = if current_piece_index
+                            == hashed_torrent_pieces.len() - 1
+                        {
+                            total_length - piece_length * (hashed_torrent_pieces.len() as u32 - 1)
+                        } else {
+                            piece_length
+                        };
+
+                        let mut payload =
+                            vec![0u8; (u32::from_be_bytes(length_bytes) - 1) as usize]; // Message takes 1 length
+                        stream.read_exact(&mut payload).await.unwrap();
+
+                        // Sanity check; why getting a piece if never asked?
+                        if current_piece_index == usize::MAX {
+                            continue;
+                        }
+
+                        if piece_buffer.is_empty() {
+                            piece_buffer = vec![0u8; this_piece_length as usize];
+                        }
+
+                        let _index = u32::from_be_bytes(payload[0..4].try_into().unwrap());
+                        let begin = u32::from_be_bytes(payload[4..8].try_into().unwrap());
+                        let block = &payload[8..];
+                        piece_buffer[begin as usize..begin as usize + block.len()]
+                            .copy_from_slice(block);
+                        block_length += block.len() as u32;
+
+                        if block_length >= this_piece_length {
+                            // Should'nt ever be greater tho but still
+                            let piece_hash = &hashed_torrent_pieces[current_piece_index];
+                            let buffer_hashed = crate::torrent::sha1_bytes_to_hex(
+                                Sha1::digest(&piece_buffer),
+                                false,
+                            )
+                            .unwrap();
+                            if piece_hash == &buffer_hashed {
+                                println!(
+                                    "<{}> has sent the piece indexed <{}>",
+                                    &ip, current_piece_index
+                                );
+                                bytes_obtained_pieces.lock().await[current_piece_index] =
+                                    piece_buffer;
+                                let done = pieces_done.fetch_add(1, Ordering::Relaxed) + 1;
+                                if done == hashed_torrent_pieces.len() {
+                                    all_done_event.notify_waiters(); // Tell every peer everything is downloaded and DC
+                                }
+                            } else {
+                                // We reject this piece, bad peer
+                                pieces_left.lock().await.push(current_piece_index);
+                            }
+                            block_length = 0;
+                            current_piece_index = usize::MAX;
+                            piece_buffer = vec![];
+                        }
+                    }
+                    [8] => (), // cancel
+                    _ => (),
+                }
+
+                if !choked && current_piece_index == usize::MAX {
+                    // Not choked and not already trying to join a piece
+                    // check if peer has piece and then send a request
+                    {
+                        let pieces = &mut *pieces_left.lock().await;
+                        if pieces.is_empty() {
+                            continue;
+                        };
+                        let piece_index = pieces.last().unwrap();
+                        if !peer_has_piece(&bitfield, *piece_index) {
+                            continue;
+                        };
+                        current_piece_index = pieces.pop().unwrap();
+                    } // piece mutex lock unlocks for other tasks
+
+                    // All pieces except MAYBE the last one have the same length, the piece can be truncated and then the last won't be the same as rest
+                    let this_piece_length =
+                        if current_piece_index == hashed_torrent_pieces.len() - 1 {
+                            total_length - piece_length * (hashed_torrent_pieces.len() as u32 - 1)
+                        } else {
+                            piece_length
+                        };
+
+                    let mut begin_offset: u32 = 0;
+
+                    while begin_offset < this_piece_length {
+                        let length = std::cmp::min(16 * 1024, this_piece_length - begin_offset);
+
+                        let length_bytes = (13u32).to_be_bytes();
+                        let message_id: [u8; 1] = [6];
+                        let mut payload = [0u8; 12];
+
+                        payload[0..4].copy_from_slice(&(current_piece_index as u32).to_be_bytes());
+                        payload[4..8].copy_from_slice(&begin_offset.to_be_bytes());
+                        payload[8..12].copy_from_slice(&length.to_be_bytes());
+
+                        stream.write_all(&length_bytes).await.unwrap();
+                        stream.write_all(&message_id).await.unwrap();
+                        stream.write_all(&payload).await.unwrap();
+
+                        begin_offset += length;
+                    }
+                } else {
+                    println!("Choked: {}", choked);
+                }
+            }
+        }));
+    }
+}
